508b2ad6d3b83f7f6fd9d933d167142c43854e00
Sam Teo <steo.59542@farnborough.ac.uk>
2018-01-17 14:35:02 +0000

    Lil bit o change

--- src/View/DJStage.java
+++ src/View/DJStage.java
@@ -1,13 +1,17 @@
 package View;
 
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import javafx.stage.WindowEvent;
 
 public class DJStage {
 
     static Pane parent;
+    public static TextField userBPM;
 
     public DJStage(Pane theParent) {
 
@@ -20,12 +24,18 @@ public class DJStage {
 
     public void start(Stage stage) {
 
-        Pane root = new Pane();
-        Scene scene = new Scene(root, 1024, 768);
-        stage.setTitle("This is another scene.");
+        VBox root = new VBox();
+        Scene scene = new Scene(root, 300, 300);
+        stage.setTitle("Auto-DJ");
         stage.setScene(scene);
         stage.setOnCloseRequest((WindowEvent we) -> closeStage(stage));
         stage.show();
+
+        Label selectBpmMessage = new Label("Please enter your desired base BPM.");
+        root.getChildren().add(selectBpmMessage);
+
+        userBPM = new TextField();
+        root.getChildren().add(userBPM);
 
     }
 
